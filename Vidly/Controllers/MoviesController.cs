afb1ad03543e6ae470e9ac606552db473d321597
Mensur Sejdinovic <mensursejdinovic@gmail.com>
2020-02-01 09:10:09 +0100

    added list of movies and customers

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,34 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie  { Id = 1, Name = "Shrek" },
+                new Movie  { Id = 2, Name = "Wall-e"}
+            };
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movies = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movies);
+        }
+
         // GET: Movies
         public ActionResult Random()
         {
