36afd9aff05a0f2902addf981b5c6deb47b8eebf
Mensur Sejdinovic <mensursejdinovic@gmail.com>
2020-02-04 13:03:51 +0100

    added genres

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,38 +5,42 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        public ViewResult Index()
-        {
-            var movies = GetMovies();
+        private ApplicationDbContext _context;
 
-            return View(movies);
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
         }
 
-        private IEnumerable<Movie> GetMovies()
+        protected override void Dispose(bool disposing)
         {
-            return new List<Movie>
-            {
-                new Movie  { Id = 1, Name = "Shrek" },
-                new Movie  { Id = 2, Name = "Wall-e"}
-            };
+            _context.Dispose();
+        }
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movies = GetMovies().SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
-            if (movies == null)
+            if (movie == null)
             {
                 return HttpNotFound();
             }
 
-            return View(movies);
+            return View(movie);
         }
+
 
         // GET: Movies
         public ActionResult Random()
