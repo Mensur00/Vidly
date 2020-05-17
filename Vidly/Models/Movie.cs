09bc417b1709e5dde46a9f0b78f9c5f880e48088
Mensur Sejdinovic <mensursejdinovic@gmail.com>
2020-05-17 21:34:22 +0200

    Build an API for renting movies

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -35,5 +35,6 @@ namespace Vidly.Models
         [Display(Name = "Number in Stock")]
         [Range(1, 20)]
         public byte NumberInStock { get; set; }
+        public byte NumberAvailable { get; set; }
     }
 }
